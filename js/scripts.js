070c436fac48d259543f4e99d52796313c64cb83
triestpa <triestpa@grinnell.edu>
2014-04-22 05:57:04 -0500

    add scroll-down button to home page

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var jumboHeight = $('.jumbotron').outerHeight();
 function parallax(){
 	var scrolled = $(window).scrollTop();
-	$('.bg').css('height', (jumboHeight-scrolled) + 'px');
+	$('.bg').css('height', (jumboHeight-scrolled) + '100px');
 }
 
 $(window).scroll(function(e){
@@ -12,4 +12,29 @@ $(".circle").hover(function(){
 	$(this).addClass("animated pulse");
 },function(){
 	$(this).removeClass("animated pulse");
+});
+
+window.onload = function() {
+	$("#home").height($(window).height()- 110);
+}
+
+
+$("#down-button").on('click', function(e) {
+	e.preventDefault();
+	var hash = this.hash;
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top - 50
+	}, 500, function(){
+		window.location.hash = hash;
+	});
+});
+
+$("#nav ul li a").on('click', function(e) {
+	e.preventDefault();
+	var hash = this.hash;
+	$('html, body').animate({
+		scrollTop: $(this.hash).offset().top - 50
+	}, 500, function(){
+		window.location.hash = hash;
+	});
 });
